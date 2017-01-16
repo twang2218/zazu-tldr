ce7dca2907bf08803b8c55b598609e5de928f95a
Tao Wang <twang2218@gmail.com>
2017-01-17 00:39:59 +1100

    Add tldr title, refactor the code and refine the test cases
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 const cache = require('tldr/lib/cache');
 
-const parser = /^- (.+)$\n\n^`([^`]+)`$/gm;
+function parseTitle(content) {
+  const parser = /^> (.+)$/gm;
+  const match = parser.exec(content);
+  return {
+    icon: 'fa-book',
+    title: `${match[1]}`,
+  };
+}
+
+function parserExamples(content) {
+  const parser = /^- (.+)$\n\n^`([^`]+)`$/gm;
+  const list = [];
+
+  let match = parser.exec(content);
+
+  while (match) {
+    const title = match[2].replace(/{{/g, '').replace(/}}/g, '');
+    list.push({
+      icon: 'fa-book',
+      title: `${title}`,
+      subtitle: match[1],
+    });
+    match = parser.exec(content);
+  }
+
+  return list;
+}
 
 module.exports = () => command => new Promise((resolve, reject) => {
   try {
-    const cmd = command.replace(' ', '-');
+    const cmd = command.trim().replace(' ', '-');
     const content = cache.getPage(cmd);
     if (!content) {
       resolve([{
@@ -13,19 +39,11 @@ module.exports = () => command => new Promise((resolve, reject) => {
       }]);
     } else {
       const list = [];
-      let match = parser.exec(content);
-
-      while (match) {
-        const title = match[2].replace(/{{/g, '').replace(/}}/g, '');
-        list.push({
-          icon: 'fa-book',
-          title: `${title}`,
-          subtitle: match[1],
-          value: `${cmd}`,
-        });
-        match = parser.exec(content);
-      }
-
+      //  Get Title
+      list.push(parseTitle(content));
+      //  Get example list
+      list.push(...parserExamples(content).map(e => Object.assign({}, e, { value: cmd })));
+      //  Return list
       resolve(list);
     }
   } catch (e) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -5,20 +5,23 @@ tldr.console = console;
 
 describe('tldr', () => {
   describe('.search', () => {
-    it('does include a name', () => {
+    it('does include a name', (done) => {
       tldr('spock').then((results) => {
         expect(results[0].title).to.include('spock');
-      });
+        done();
+      }).catch(done);
     });
-    it('should find doc for "rm"', () => {
+    it('should find doc for "rm"', (done) => {
       tldr('rm').then((results) => {
-        expect(results[0].title).to.include('another');
-      });
+        expect(results[1].title).to.include('another');
+        done();
+      }).catch(done);
     });
-    it('should find doc "git rm"', () => {
+    it('should find doc "git rm"', (done) => {
       tldr('git rm').then((results) => {
-        expect(results[0].title).to.include('git rm file');
-      });
+        expect(results[1].title).to.include('git rm file');
+        done();
+      }).catch(done);
     });
   });
 });
